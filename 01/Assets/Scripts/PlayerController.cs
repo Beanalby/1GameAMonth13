47f231d5eeb2d535c2a2781531e8e2017adf26c9
Jason Viers <bean@beanalby.net>
2013-01-15 21:51:26 -0500

    Added charging, improved initial ProgressCircle display

--- 01/Assets/Scripts/PlayerController.cs
+++ 01/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     private float walkSpeed = 5f;
-    private float fireSpeed = 1f;
+    private float chargeTime = 1f;
 
     private CapsuleCollider cc;
     private BoardController board;
@@ -13,59 +13,71 @@ public class PlayerController : MonoBehaviour {
     public GameObject ProgressCircleTemplate;
     private ProgressCircle pc;
     private float pcStart;
+    private SquareController pcTarget;
 
-	// Use this for initialization
 	void Start () {
         cc = GetComponent<CapsuleCollider>();
         board = GameObject.Find("Board").GetComponent<BoardController>();
         pcStart = -1;
 	}
 	
-	// Update is called once per frame
 	void Update () {
         UpdateMovement();
         HandleFiring();
-        if (pcStart != -1)
-        {
-            if (Time.time > pcStart + fireSpeed)
-            {
-                Destroy(pc.gameObject);
-                pcStart = -1;
-            }
-            else
-            {
-                float percent = Mathf.InverseLerp(pcStart + fireSpeed, pcStart, Time.time);
-                Debug.Log("Setting to " + percent);
-                pc.percent = percent;
-            }
-        }
     }
 
     void HandleFiring()
     {
-        if (!Input.GetButtonDown("Jump"))
-            return;
-        // see what's beneath us
-        RaycastHit hit;
-        Vector3 pos = transform.position;
-        pos.y += .1f;
-        if (Physics.Raycast(pos, -Vector3.up, out hit, cc.height * 1.1f))
+        if (pcStart == -1 && Input.GetButtonDown("Jump"))
         {
-            SquareController sc = hit.collider.gameObject.GetComponent<SquareController>();
-            if (sc != null)
+            // started charging, grab the target square
+            RaycastHit hit;
+            Vector3 pos = transform.position;
+            pos.y += .1f;
+            if (Physics.Raycast(pos, -Vector3.up, out hit, cc.height * 1.1f))
             {
-                board.SquareHit(sc);
-                pc = ((GameObject)GameObject.Instantiate(ProgressCircleTemplate)).GetComponent<ProgressCircle>();
-                pos = sc.transform.position;
-                pos.y += .1f;
-                pc.transform.position = pos;
-                pc.transform.rotation = Quaternion.Euler(new Vector3(90, 180, 0));
-                pcStart = Time.time - .01f;
+                pcTarget = hit.collider.gameObject.GetComponent<SquareController>();
+                if (pcTarget != null)
+                {
+                    pc = ((GameObject)GameObject.Instantiate(ProgressCircleTemplate)).GetComponent<ProgressCircle>();
+                    pos = pcTarget.transform.position;
+                    pos.y += .1f;
+                    pc.transform.position = pos;
+                    pc.transform.rotation = Quaternion.Euler(new Vector3(90, 180, 0));
+                    pcStart = Time.time;
+                    //Debug.Break();
+                }
             }
+            return;
+        }
+        else if (pcStart != -1)
+        {
+            if (Input.GetButtonUp("Jump"))
+            {
+                // they let go, cancel charging
+                Destroy(pc.gameObject);
+                pcStart = -1;
+                return;
+            }
+            if (Time.time > pcStart + chargeTime)
+            {
+                // finished charging, DOO EET
+                Destroy(pc.gameObject);
+                pcStart = -1;
+                board.SquareHit(pcTarget);
+                return;
+            }
+            // still charging, update the progress
+            float percent = Mathf.InverseLerp(pcStart, pcStart + chargeTime, Time.time);
+            pc.percent = percent;
+            return;
         }
     }
     void UpdateMovement()
     {
+        // don't allow any movement if they're charging
+        if (pcStart != -1)
+            return;
         float h = Input.GetAxis("Horizontal"), v = Input.GetAxis("Vertical");
         Vector3 pos = transform.position;
         pos.x += Time.deltaTime * walkSpeed * h;
@@ -76,9 +88,9 @@ public class PlayerController : MonoBehaviour {
         pos.x = Mathf.Max(bounds.x+cc.radius, (Mathf.Min(bounds.width-cc.radius, pos.x)));
         pos.z = Mathf.Max(bounds.y+cc.radius, (Mathf.Min(bounds.height-cc.radius, pos.z)));
 
-        if (h != 0 || v != 0)
+        Vector3 lookTarget = new Vector3(h, transform.position.y, v);
+        if (lookTarget != Vector3.zero)
         {
-            Vector3 lookTarget = new Vector3(h, transform.position.y, v);
             transform.rotation = Quaternion.LookRotation(lookTarget);
             transform.position = pos;
         }
--- 01/Assets/Scripts/ProgressCircle.cs
+++ 01/Assets/Scripts/ProgressCircle.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 public class ProgressCircle : MonoBehaviour {
-    public float percent = 0;
+    public float percent = 1;
+    void Start() {
+        renderer.materials[1].SetFloat("_Cutoff", 1);
+    }
 	void Update () {
-        renderer.materials[1].SetFloat("_Cutoff", percent);
+        renderer.materials[1].SetFloat("_Cutoff", 1-percent);
 	}
 }
