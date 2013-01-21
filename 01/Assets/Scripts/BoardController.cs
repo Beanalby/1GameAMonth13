d1e892c773ce9f58b0673c3c9a64357c9cfd3a75
Jason Viers <bean@beanalby.net>
2013-01-20 21:53:21 -0500

    Switched board configuration from 2d int[,] to 1d List<int>, added IsSolvable, squares now track IsCorrupted instead of IsLight

--- 01/Assets/Scripts/BoardController.cs
+++ 01/Assets/Scripts/BoardController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -6,53 +7,115 @@ public class BoardController : MonoBehaviour {
 
     public GameObject squareTemplate;
 
-    private int boardSize = 5;
+    public static int BoardSize = 5;
     private float squareX, squareZ;
 
-    public SquareController[,] squares;
+    private List<SquareController> squares;
 
-	// Use this for initialization
+    private List<int> silentPattern1, silentPattern2, silentPattern3;
+
 	void Start() {
         squareX = squareTemplate.GetComponent<BoxCollider>().size.x;
         squareZ = squareTemplate.GetComponent<BoxCollider>().size.z;
 
-        squares = new SquareController[boardSize, boardSize];
-        for(int i = 0; i < boardSize; i++) {
-            for (int j = 0; j < boardSize; j++) {
-                squares[i,j] = ((GameObject)GameObject.Instantiate(squareTemplate)).GetComponent<SquareController>();
-                squares[i,j].gameObject.name = "sq" + i + j;
-                squares[i, j].boardX = i; squares[i, j].boardY = j;
-                squares[i,j].transform.parent = transform;
-                squares[i,j].transform.position = new Vector3(
-                    squares[i,j].transform.position.x + (i+.5f) * squareX,
-                    squares[i,j].transform.position.y,
-                    squares[i,j].transform.position.z + (j+.5f) * squareZ);
-            }
+        squares = new List<SquareController>(BoardSize * BoardSize);
+        for(int i = 0; i < BoardSize*BoardSize; i++) {
+            squares.Add(((GameObject)GameObject.Instantiate(squareTemplate)).GetComponent<SquareController>());
+            squares[i].boardIndex = i;
+            squares[i].transform.parent = transform;
+        }
+        InitSilentPatterns();
+        List<List<int>> testBoards = new List<List<int>>() {
+            new List<int>() { 0 },
+            new List<int>() { 0,1 },
+            new List<int>() { 0,2 }
+        };
+
+        foreach(List<int> board in testBoards) {
+            Debug.Log(IsSolvable(board) + " for " + board);
         }
 	}
+    void InitSilentPatterns() {
+        /* XX.XX
+         * .....
+         * XX.XX
+         * .....
+         * XX.XX */
+        silentPattern1 = new List<int>() {
+            0,  1,  3,  4,
+            10, 11, 13, 14,
+            20, 21, 23, 24
+        };
+        /* X.X.X
+         * X.X.X
+         * .....
+         * X.X.X
+         * X.X.X */
+        silentPattern2 = new List<int>() {
+            0,  2,  4,
+            5,  7,  9,
+            15, 17, 19,
+            20, 22, 24
+        };
+        /* 3 is combination of first two
+         * .XXX.
+         * X.X.X
+         * XX.XX
+         * X.X.X
+         * .XXX. */
+        silentPattern3 = new List<int>() {
+            1, 2, 3,
+            5, 7, 9,
+            10, 11, 13, 14,
+            15, 17, 19,
+            21, 22, 23
+        };
+    }
 
     public Rect GetBounds() {
-        return new Rect(0, 0, squareX * boardSize, squareZ * boardSize);
+        return new Rect(0, 0, squareX * BoardSize, squareZ * BoardSize);
+    }
+
+
+    private List<int> Board2list() {
+        List<int> boardState = new List<int>();
+        foreach(SquareController sc in squares) {
+            if(sc.IsCorrupted) {
+                boardState.Add(sc.boardIndex);
+            }
+        }
+        return boardState;
+    }
+    public bool IsSolvable(List<int> boardState) {
+        /* a board is unsolvable if either of the silent patterns
+         * have an odd number marked. */
+        if(boardState.Intersect(silentPattern1).Count() % 2 != 0) {
+            return false;
+        }
+        if(boardState.Intersect(silentPattern2).Count() % 2 != 0) {
+            return false;
+        }
+        return true;
     }
 
     public void SquareHit(SquareController sc) {
         sc.Toggle();
         // toggle any additional squares around them
-        if(sc.boardX > 0)
-            squares[sc.boardX - 1, sc.boardY].Toggle();
-        if(sc.boardX < boardSize - 1)
-            squares[sc.boardX + 1, sc.boardY].Toggle();
         if(sc.boardY > 0)
-            squares[sc.boardX, sc.boardY - 1].Toggle();
-        if(sc.boardY < boardSize - 1)
-            squares[sc.boardX, sc.boardY + 1].Toggle();
+            squares[sc.boardIndex-1].Toggle();
+        if(sc.boardY < BoardSize - 1)
+            squares[sc.boardIndex+1].Toggle();
+        if(sc.boardX > 0)
+            squares[sc.boardIndex-BoardSize].Toggle();
+        if(sc.boardX < BoardSize - 1)
+            squares[sc.boardIndex+BoardSize].Toggle();
     }
 
     public void OnDrawGizmos() {
         Vector3 size = squareTemplate.GetComponent<BoxCollider>().size;
         Gizmos.color = Color.green / 2;
-        for(int i = 0; i < boardSize; i++) {
-            for(int j = 0; j < boardSize; j++) {
+        for(int i = 0; i < BoardSize; i++) {
+            for(int j = 0; j < BoardSize; j++) {
                 Vector3 pos = new Vector3((i + .5f) * size.x, -.5f * size.y, (j + .5f) * size.z);
                 Gizmos.DrawWireCube(pos, size);
             }
--- 01/Assets/Scripts/SquareController.cs
+++ 01/Assets/Scripts/SquareController.cs
@@ -6,30 +6,49 @@ public class SquareController : MonoBehaviour {
     public Material lightMaterial;
     public Material darkMaterial;
 
-    public int boardX, boardY;
+    public int boardIndex;
 
-    private bool isLight = false;
+    private bool isCorrupted = false;
     private MeshRenderer mr;
     
 	// Use this for initialization
 	void Start() {
+        InitIndex();
         mr = GetComponentInChildren<MeshRenderer>();
         // we might've already been toggled when created
         UpdateMaterial();
 	}
-	
+    private void InitIndex() {
+        float width = GetComponentInChildren<BoxCollider>().size.x;
+        float length = GetComponentInChildren<BoxCollider>().size.z;
+        gameObject.name = "sq" + boardX + boardY;
+        transform.position = new Vector3(
+            transform.position.x + (boardX + .5f) * width,
+            transform.position.y,
+            transform.position.z + (boardY + .5f) * length);
+    }
+
     public void Toggle() {
-        isLight = !isLight;
+        isCorrupted = !isCorrupted;
         UpdateMaterial();
     }
     private void UpdateMaterial() {
         if(mr == null) { // might not be Start()ed yet
             return;
         }
-        if(isLight) {
-            mr.material = lightMaterial;
-        } else {
+        if(isCorrupted) {
             mr.material = darkMaterial;
+        } else {
+            mr.material = lightMaterial;
         }
+    }
+    public bool IsCorrupted {
+        get { return isCorrupted; }
+    }
+    public int boardX {
+        get { return (int)(Mathf.Floor(boardIndex / BoardController.BoardSize)); }
+    }
+    public int boardY {
+        get { return boardIndex % 5; }
     }
 }
