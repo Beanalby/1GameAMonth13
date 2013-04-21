6176efd80e24dd631ba7119bdf757bdf14472ffa
Jason Viers <bean@beanalby.net>
2013-04-21 17:39:29 -0400

    Added treadmill functionality

--- unity/Assets/04/Scripts/Player.cs
+++ unity/Assets/04/Scripts/Player.cs
@@ -1,33 +1,51 @@
 using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(BoxCollider))]
 public class Player : MonoBehaviour {
 
-    //Vector3 velocity;
-
+    private float colWidth;
     private float distanceToGround = .5f;
-    private float jumpSpeed = 10;
+    private float jumpSpeed = 7f;
+    private float treadmillSpeed = 2f;
 
     bool isGrounded {
         get {
-            return Physics.Raycast(new Ray(transform.position, Vector3.down), distanceToGround+.1f);
+            return Physics.Raycast(new Ray(transform.position, Vector3.down), distanceToGround + .1f);
         }
+    }
+
+    void Start() {
+        colWidth = GetComponent<BoxCollider>().bounds.size.x;
     }
     void Update() {
         Vector3 velocity = rigidbody.velocity;
-        velocity.x = Input.GetAxis("Horizontal");
+        // check if either our left or right edge is over a treadmill
+        if(rigidbody.velocity.y <= 0.1) {
+            Vector3 pos = transform.position;
+            float dist = distanceToGround + .1f;
+            bool isOnTreadmill = false;
+            foreach(float offset in new float[] { colWidth, -colWidth }) {
+                pos = transform.position;
+                pos.x += offset;
+                Ray ray = new Ray(pos, Vector3.down);
+                foreach(RaycastHit hit in Physics.RaycastAll(ray, dist)) {
+                    if(hit.collider.tag == "Treadmill") {
+                        isOnTreadmill = true;
+                    }
+                }
+                if(isOnTreadmill) {
+                    break;
+                }
+            }
+            if(isOnTreadmill) {
+                velocity.x = treadmillSpeed;
+            }
+        }
+
         if(Input.GetButtonUp("Jump")) {
             velocity.y = jumpSpeed;
         }
         rigidbody.velocity = velocity;
-        //if(!isGrounded) {
-        //    velocity += Physics.gravity * Time.deltaTime;
-        //}
-
-        //rigidbody.MovePosition(rigidbody.position + velocity * Time.deltaTime);
-    }
-    void OnCollisionEnter(Collision collision) {
-        Debug.Log("Collided at " + collision.contacts[0].point);
-        //velocity.y = 0;
     }
 }
