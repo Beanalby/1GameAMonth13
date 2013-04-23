8268658d167a658446329a5285c7eb5589d95b9e
Jason Viers <bean@beanalby.net>
2013-04-23 02:04:03 -0400

    Added CameraSwapY

---
+++ unity/Assets/04/Scripts/CameraSwapY.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraSwapY : MonoBehaviour {
+    public GameObject topCamera;
+    public GameObject bottomCamera;
+
+    public void OnTriggerEnter(Collider other) {
+        // only swap for the player
+        Player p = other.GetComponent<Player>();
+        if(p == null || p.IsDead) {
+            return;
+        }
+        if(p.rigidbody==null) {
+            Debug.LogError("!!! No rigidbody on player!");
+            return;
+        }
+        // switch camera based on which direction they're heading
+        if(other.rigidbody.velocity.y >= 0) {
+            CameraManager.instance.Current = topCamera;
+        } else {
+            CameraManager.instance.Current = bottomCamera;
+        }
+    }
+}
