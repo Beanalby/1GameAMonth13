cadf9169d16be10a25d58075dcb411879f7a0e2c
Jason Viers <bean@beanalby.net>
2013-06-16 17:44:53 -0400

    TextCreater can now be used without instantiating the prefab

--- unity/Assets/06/Scripts/TextCreator.cs
+++ unity/Assets/06/Scripts/TextCreator.cs
@@ -10,27 +10,26 @@ public class TextCreator : MonoBehaviour {
     Dictionary<char, GameObject> letters;
 
     public void Awake() {
-        Debug.Log("Starting TextCreator Awake");
+        InitLetters();
+    }
+
+    private void InitLetters() {
         letters = new Dictionary<char, GameObject>();
         foreach(Transform t in TextMesh.transform) {
             letters[(char)int.Parse(t.name)] = t.gameObject;
-            Debug.Log("Added in [" + (char)int.Parse(t.name)
-                + "] (" + int.Parse(t.name)+")");
         }
-        Debug.Log("Finished awake, populated with " + letters.Keys.Count + " letters");
     }
     public GameObject[] GetText(Transform parent, string text) {
+        if(letters == null) {
+            InitLetters();
+        }
         float step = .4f;
         float baseOffset = -step * text.Length / 2;
         GameObject[] ret = new GameObject[text.Length];
-        Debug.Log("pulling from " + letters.Keys.Count + " letters");
         for(int i=0;i<text.Length;i++) {
             if(text[i] == ' ') {
                 continue;
             }
-            Debug.Log("Pulling [" + text[i]);
-            GameObject obj = letters[text[i]];
-            Debug.Log("Pulled " + obj + " for " + text[i]);
             ret[i] = (GameObject)Instantiate(letters[text[i]]);
             ret[i].transform.parent = parent;
             ret[i].transform.localPosition = new Vector3(baseOffset + step * i, 0, 0);
