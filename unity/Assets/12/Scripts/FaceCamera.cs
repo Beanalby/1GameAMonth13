843b349351901e73a1d524fcb454f6fc6628e7f0
Jason Viers <bean@beanalby.net>
2013-12-14 22:30:49 -0500

    Added PlanetCute assets

---
+++ unity/Assets/12/Scripts/FaceCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class FaceCamera : MonoBehaviour {
+
+    Transform mesh, cam;
+
+    public void Start() {
+        mesh = transform.Find("mesh");
+        cam = Camera.main.transform;
+    }
+    public void Update() {
+        mesh.rotation = cam.rotation;
+    }
+}
